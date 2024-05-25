ca1b047941662334ab747b9a8c4ccc07659dcee7
Uhan7 <68632441+Uhan7@users.noreply.github.com>
2024-05-25 23:00:45 +0800

    Revert "2024May23_GDPROG2_Seatwork"
    This reverts commit 9f9945701c7026d980ae51b982883fb29555cfbe.

--- encounter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void showItem1();
-void showItem2();
-void showItem3();
-void playerDamageHP();
-int checkInventory(int item, int inventorySize, int* inventory);
-void runEncounter(int* playerHP, int inventorySize, int* inventory);
-void displayEncounter(int* playerHP, int inventorySize, int* inventory);
--- inventory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void runInventory(int* inventory, int inventorySize);
-void displayInventory(int* inventory, int inventorySize);
-void displayEmptyBox(int line);
-void displayItem1(int line);
-void displayItem2(int line);
-void displayItem3(int line);
